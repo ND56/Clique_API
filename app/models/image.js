15d2ff7945ad8adb19451be91eb8653c7e06c8b3
ND56 <ndrew56@gmail.com>
2018-03-23 19:44:11 -0400

    Created images routes, model, and controller.
    NEXT STEPS: Adding aws upload code and then testing create action.

---
+++ app/models/image.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+// eventually add a comment_id field (like the owner field)
+const imageSchema = new mongoose.Schema({
+  filename: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  tags: [{
+    content: String
+  }],
+  _owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true,
+  toJSON: {
+    transform: function (doc, ret, options) {
+      const userId = (options.user && options.user._id) || false
+      ret.editable = userId && userId.equals(doc._owner)
+      return ret
+    }
+  }
+})
+
+const Image = mongoose.model('Image', imageSchema)
+
+module.exports = Image
